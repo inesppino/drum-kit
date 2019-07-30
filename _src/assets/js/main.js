783c206d0b3e25916ac21fea289db6c7ec80c61a
Inés Pedraza Pino <inesppino@MacBook-Pro-de-Ines.local>
2019-07-30 09:22:20 +0200

    update npm

--- _src/assets/js/main.js
+++ _src/assets/js/main.js
@@ -1,17 +1,17 @@
 'use strict';
 
 function playSound(e) {
-    const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`); //selecciona dentro de la etiqueta audio el data-key detectado por el evento
-    const key = document.querySelector(`.key[data-key="${e.keyCode}"]`);// selecciona dentro de la clase .key el data-key que viene por el evento
-    if(!audio) return; //detiene la funcion si no se detecta ningun keyCode relacionado con un audio
-    audio.currentTime = 0; //se setea el tiempo a 0 para poder pulsar repetidas veces las teclas sin esperar a que termine el audio ni la transicion
-    audio.play(); //ejecuta el audio
-    key.classList.add('playing'); //añade la clase
+  const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`); //selecciona dentro de la etiqueta audio el data-key detectado por el evento
+  const key = document.querySelector(`.key[data-key="${e.keyCode}"]`);// selecciona dentro de la clase .key el data-key que viene por el evento
+  if(!audio) return; //detiene la funcion si no se detecta ningun keyCode relacionado con un audio
+  audio.currentTime = 0; //se setea el tiempo a 0 para poder pulsar repetidas veces las teclas sin esperar a que termine el audio ni la transicion
+  audio.play(); //ejecuta el audio
+  key.classList.add('playing'); //añade la clase
 }
 
 function removeTrasition(e) {
-    if(e.propertyName !== 'transform') return; //esto avisa cuando la transformación (de la clase) ha terminado
-    this.classList.remove('playing');
+  if(e.propertyName !== 'transform') return; //esto avisa cuando la transformación (de la clase) ha terminado
+  this.classList.remove('playing');
 }
 
 window.addEventListener('keydown', playSound);
