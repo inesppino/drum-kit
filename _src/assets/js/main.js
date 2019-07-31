2a4ff11cad80e61ac85fe987b4d5e0837d7197c4
Inés Pedraza Pino <inesppino@mbp-de-ines.dhcp.mano.es.sopra>
2019-07-31 08:44:56 +0200

    ntc

--- _src/assets/js/main.js
+++ _src/assets/js/main.js
@@ -2,7 +2,7 @@
 
 function playSound(e) {
   const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`); //selecciona dentro de la etiqueta audio el data-key detectado por el evento
-  const key = document.querySelector(`.key[data-key="${e.keyCode}"]`);// selecciona dentro de la clase .key el data-key que viene por el evento
+  const key = document.querySelector(`.key[data-key="${e.keyCode}"]`); // selecciona dentro de la clase .key el data-key que viene por el evento
   if(!audio) return; //detiene la funcion si no se detecta ningun keyCode relacionado con un audio
   audio.currentTime = 0; //se setea el tiempo a 0 para poder pulsar repetidas veces las teclas sin esperar a que termine el audio ni la transicion
   audio.play(); //ejecuta el audio
@@ -10,7 +10,7 @@ function playSound(e) {
 }
 
 function removeTrasition(e) {
-  if(e.propertyName !== 'transform') return; //esto avisa cuando la transformación (de la clase) ha terminado
+  if(e.propertyName !== 'transform') return; //esto avisa cuando la transformación de la clase ha terminado
   this.classList.remove('playing');
 }
 
